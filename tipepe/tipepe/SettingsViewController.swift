83e714894687a2b07ad523310db024e852561155
limitlessfractals <fanwuq@gmail.com>
2017-01-07 10:19:37 -0600

    Add files via upload

---
+++ tipepe/tipepe/SettingsViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SettingsViewController.swift
+//  tipepe
+//
+//  Created by Wuqiong Fan on 1/7/17.
+//  Copyright © 2017 Wuqiong Fan. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    // @IBOutlet weak var dBill: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+   // @IBAction func onTap(_ sender: Any) {
+   //     view.endEditing(true)
+//    }
+    
+    // set default value to what's in the text field
+  //  @IBAction func setDefault(_ sender: Any) {
+//        let defaults = UserDefaults.standard
+  
+//        let dBills = Double(dBill.text!) ?? 0
+        
+//        defaults.set(dBills, forKey: "myDouble")
+//        defaults.synchronize()
+//    }
+
+}
--- tipepe/tipepe/ViewController.swift
+++ tipepe/tipepe/ViewController.swift
@@ -2,14 +2,15 @@
 //  ViewController.swift
 //  tipepe
 //
-//  Created by Ikkyu on 1/5/17.
-//  Copyright © 2017 Ikkyu. All rights reserved.
+//  Created by Wuqiong Fan on 1/5/17.
+//  Copyright © 2017 Wuqiong Fan. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    // ctrl + drag elements to here to initialize them
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
@@ -26,24 +27,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // dismisses keyboard on tap
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
+    // function that calculates the tip
     @IBAction func calculateTip(_ sender: Any) {
         
         let tipPercentages = [0.15, 0.18, 0.2, 0.25]
         
+        //load defaults from settings
+        let defaults = UserDefaults.standard
+        let dBill = defaults.double(forKey: "myDouble")
         
-        let bill = Double(billField.text!) ?? 0
+        // convert string to double and set default to 0
+        let bill = Double(billField.text!) ?? dBill
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
+        // set text field values as strings of the number to 2 decimal places
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
+    
+    // allows instant update with Tip Control changes
+    @IBAction func vaChange(_ sender: Any) {
+        self.calculateTip(Any.self)
+    }
 
+    
+    
 }
 
 
